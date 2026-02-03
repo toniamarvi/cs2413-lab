051836c05e06807ead9482d00f7e9923b55b8ad3
toniamarvi <toniamarvi06@gmail.com>
2026-02-03 09:20:29 -0600

    two_sum_sorted first try

--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -4,6 +4,22 @@ int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
     if (!nums || !out_i || !out_j || n < 2) return 0;
 
     // TODO: implement rwo sum solution for sorted arrays.
+    /* have two pointers, one at the beginning and one at the end, loop through the array and increment by one,
+    if the sum of the values at the two pointers is too small, move left pointer right and vice versa if its too big */
+
+    int left = 0;
+    int right = n-1;
+
+    for (int i=0;left<right;i++) {
+        if (nums[left] + nums[right] == target) {
+            *out_i = left;
+            *out_j = right;
+        } else if (nums[left] + nums[right] > target) {
+            right--;
+        } else if (nums[left] + nums[right] < target) {
+            left++;
+        }
+    }
 
     return 0;
 }
