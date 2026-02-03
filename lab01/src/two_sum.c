29b59b3e2fd482c4909a50f595dd645458c500e7
toniamarvi <toniamarvi06@gmail.com>
2026-02-03 08:53:13 -0600

    two sum pt.3

--- lab01/src/two_sum.c
+++ lab01/src/two_sum.c
@@ -7,9 +7,8 @@ int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
     //  - find i < j such that nums[i] + nums[j] == target
     //  - store indices in *out_i and *out_j
     //  - return 1 if found; otherwise return 0
-    int length = sizeof(nums) / sizeof(nums[0]);
-    for (int i=0;i<length;i++) {
-        for (int j=i+1;j<length;i++) {
+    for (int i=0;i<n;i++) {
+        for (int j=i+1;j<n;j++) {
             if (nums[i] + nums[j] == target) {
                 *out_i = i;
                 *out_j = j;
